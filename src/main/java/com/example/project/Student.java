ce44ee2c492c06d66a23f5f6c521e4f7e1606494
cherinesoe <148895697+cherinesoe@users.noreply.github.com>
2024-10-01 12:40:51 +0000

    student

--- src/main/java/com/example/project/Student.java
+++ src/main/java/com/example/project/Student.java
@@ -8,43 +8,53 @@ public class Student {
     // sum of test scores (double).. should initialize at 0.0
     // test score count (int) ..should initialize at 0
     // highest test score (double).. should initialize at 0.0
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double accumulatedTestScores = 0.0;
+    private int testCount = 0;
+    private double highestScore = 0.0;
  
     // constructor
     public Student(String firstName, String lastName, int gradYear) {
         //implement code here!
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
 
     }
  
     // returns firstName
     public String getFirstName() {
         //implement code here!
-        return "";
+        return firstName;
     }
  
     // returns lastName
     public String getLastName() {
         //implement code here!
-        return "";
+        return lastName;
     }
  
     public double getHighestTestScore() {
         //implement code here!
-        return 0.0;
+        return highestScore;
     }
 
     public int getTestScoreCount(){
         //implement code here!
-        return 0;
+        return testCount;
     }
 
     public int getGradYear(){
         //implement code here!
-        return 0;
+        return gradYear;
     }
  
     // sets gradYear to newGradYear
     public void setGradYear(int newGradYear) {
         //implement code here!
+        gradYear = newGradYear;
     }
  
     // adds newTestScore to accumulatedTestScores
@@ -52,26 +62,35 @@ public class Student {
     // set new highest test score 
     public void addTestScore(double newTestScore) {
         //implement code here!
+        accumulatedTestScores += newTestScore;
+        testCount++;
+        if (newTestScore > highestScore){
+            highestScore = newTestScore;
+        }
     }
  
     // returns true if the student's average test score is greater
     // than or equal to 65; returns false otherwise (see Note 2 below)
     public boolean isPassing() {
         //implement code here!
-        return false;
+        if ((accumulatedTestScores / testCount) >= 65) {
+            return true;
+        } else {
+            return false;
+        }
     }
  
     // returns the Student's average test score as the
     // quotient of accumulatedTestScores and testScoreCount
     public double averageTestScore() {
         //implement code here!
-        return 0.0;
+        return accumulatedTestScores / testCount;
     }
  
     // this method prints all info of a Student object to the console 
     // I AM NOT TESTING YOU ON THIS METHOD. IT'S FOR YOUR TESTING PURPOSES ONLY.. you don't have to use it
     public void printStudentInfo() {
-        System.out.println("Student Full Name: " );
+        System.out.println("Student Full Name: ");
         System.out.println("Graduation Year: ");
         System.out.println("Number of tests: ");
         System.out.println("Average Test Score: ");
